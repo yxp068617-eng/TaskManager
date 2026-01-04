419ecc40a51e971074674e88302c4689052d6350
y123 <yxp068617@outlook.com>
2026-01-04 11:21:12 +0800

    feat: 实现TaskModel自定义数据模型

--- TaskModel.h
+++ TaskModel.h
@@ -1,10 +1,26 @@
 #ifndef TASKMODEL_H
 #define TASKMODEL_H
 
-class TaskModel
+#include <QSqlRelationalTableModel>
+#include <QVariant>
+#include <QBrush>
+#include "TaskDatabase.h"
+
+class TaskModel : public QSqlRelationalTableModel
 {
+    Q_OBJECT
 public:
-    TaskModel();
+    explicit TaskModel(QObject *parent = nullptr, QSqlDatabase db = QSqlDatabase());
+
+    // 重写data方法：自定义显示（优先级中文、完成状态颜色）
+    QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const override;
+
+    // 重写headerData：设置列名
+    QVariant headerData(int section, Qt::Orientation orientation, int role = Qt::DisplayRole) const override;
+
+private:
+    // 优先级转中文
+    QString priorityToText(TaskPriority priority) const;
 };
 
 #endif // TASKMODEL_H
